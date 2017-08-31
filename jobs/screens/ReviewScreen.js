3c0d7a107e87d05c3138d81939ba29e461923eb3
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-08-31 15:41:26 +0700

    install react-native-elements and programmatic navigation

--- jobs/screens/ReviewScreen.js
+++ jobs/screens/ReviewScreen.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
+import { Button } from 'react-native-elements'
 
 class ReviewScreen extends Component {
     // The block of code will automatically moved into the constructor of the component - instance property
     //state = { color: 'red' }
-    static navigationOptions = {
+    static navigationOptions = ({ navigation }) => ({
         title: 'Review Jobs',
-        headerRight: <Text>Go Right</Text>
-    }
+        headerRight: <Button title="Settings" onPress={() => { navigation.navigate('settings') }} />
+    })
 
     render() {
         return (
