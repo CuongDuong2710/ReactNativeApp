a00e707b3b0cc3f23a62912e9242e1c68b2cd659
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-08-31 15:14:35 +0700

    adding title and headerRight

--- jobs/screens/ReviewScreen.js
+++ jobs/screens/ReviewScreen.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 
 class ReviewScreen extends Component {
+    // The block of code will automatically moved into the constructor of the component - instance property
+    //state = { color: 'red' }
+    static navigationOptions = {
+        title: 'Review Jobs',
+        headerRight: <Text>Go Right</Text>
+    }
+
     render() {
         return (
             <View>
