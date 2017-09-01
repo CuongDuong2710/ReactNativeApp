7cbb3773844bb0d7c24f509b38c26c18c632f04f
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-09-01 08:44:33 +0700

    styling welcome screen

--- jobs/components/Slides.js
+++ jobs/components/Slides.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 
 class Slides extends Component {
+  renderSlides() {
+    // Every single object that's inside of props stack data will create a new view contain the slide text
+    // 'slide' is { text: 'Welcome to JobApp' } object in 'map' function
+    return this.props.data.map((slide) => {
+      return (
+        <View key={slide.text} style={styles.slideStyle}>
+          <Text style={styles.slideText}>{slide.text}</Text>
+        </View>
+      )
+    })
+  }
+
   render() {
     return (
-      <View>
-      </View>
+      <ScrollView
+        horizontal
+        style={{ flex: 1}}
+      >
+        {this.renderSlides()}
+      </ScrollView>
     )
+  }
+}
+
+const styles = {
+    slideStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  slideText: {
+    fontSize: 30  
   }
 }
 
