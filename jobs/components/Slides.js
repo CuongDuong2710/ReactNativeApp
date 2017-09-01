6e0d8302f4458e451afbe3a6e98822f3084a52b8
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-09-01 09:10:02 +0700

    more styling welcome screen

--- jobs/components/Slides.js
+++ jobs/components/Slides.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
-import { View, Text, ScrollView } from 'react-native'
+import { View, Text, ScrollView, Dimensions } from 'react-native'
+
+// Get width of screen
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 class Slides extends Component {
   renderSlides() {
@@ -7,18 +10,23 @@ class Slides extends Component {
     // 'slide' is { text: 'Welcome to JobApp' } object in 'map' function
     return this.props.data.map((slide) => {
       return (
-        <View key={slide.text} style={styles.slideStyle}>
+        <View 
+          key={slide.text}
+          style={[styles.slideStyle, { backgroundColor: slide.color }]}
+        >
           <Text style={styles.slideText}>{slide.text}</Text>
         </View>
       )
     })
   }
 
+  // 'pagingEnabled': automatically advance to closest page when user drag halfway
   render() {
     return (
       <ScrollView
         horizontal
         style={{ flex: 1}}
+        pagingEnabled
       >
         {this.renderSlides()}
       </ScrollView>
@@ -26,14 +34,18 @@ class Slides extends Component {
   }
 }
 
+// 'textAlign: center' is just for Android multitext
 const styles = {
     slideStyle: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    width: SCREEN_WIDTH
   },
   slideText: {
-    fontSize: 30  
+    fontSize: 30,
+    color: 'white',
+    textAlign: 'center'
   }
 }
 
