4e5fe344752bf0c5707825c008717de3033b423c
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-09-01 09:39:08 +0700

    adding button last slide

--- jobs/components/Slides.js
+++ jobs/components/Slides.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react'
 import { View, Text, ScrollView, Dimensions } from 'react-native'
+import { Button } from 'react-native-elements'
 
 // Get width of screen
 const SCREEN_WIDTH = Dimensions.get('window').width;
 
 class Slides extends Component {
+  // render button on last slide
+  // 'raised': default styling give button a slightly better look
+  renderLastSlide(index) {
+    if (index === this.props.data.length - 1) {
+        return (
+          <Button
+            title="Onwards!"
+            raised
+          />
+        )
+    }
+  }
+  // render each slides of Welcome screen
   renderSlides() {
     // Every single object that's inside of props stack data will create a new view contain the slide text
-    // 'slide' is { text: 'Welcome to JobApp' } object in 'map' function
-    return this.props.data.map((slide) => {
+    // 'slide' is record object of 'data' in 'map' function, ex: { text: 'Welcome to JobApp' }
+    return this.props.data.map((slide, index) => {
       return (
         <View 
           key={slide.text}
           style={[styles.slideStyle, { backgroundColor: slide.color }]}
         >
           <Text style={styles.slideText}>{slide.text}</Text>
+          {this.renderLastSlide(index)}
         </View>
       )
     })
