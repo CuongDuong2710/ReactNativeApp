b6e1a65deb6b616b895adb4a326b20dbe99dcbc0
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-09-03 15:31:13 +0700

    crazying async/await syntax

--- jobs/actions/auth_actions.js
+++ jobs/actions/auth_actions.js
@@ -8,7 +8,13 @@ import {
 // AsyncStorage.setItem('fb_token', token)
 // AsyncStorage.getItem('fb_token')
 
-// The keyword 'async' here means it takes some amount of time to read this value of the device's memory
-export const facebookLogin = () => {
-    console.log(AsyncStorage.getItem('fb_token'))
+// The keyword 'async' here means it takes some amount of time to read this value of the device's memory.
+// This is not a synchronous call so we cannot say.
+export const facebookLogin = () => async dispatch => {
+  let token = await AsyncStorage.getItem('fb_token')
+    if (token) {
+        // Dispatch an action saying FB login is done
+    } else {
+        // Start up FB login process
+  }
 }
