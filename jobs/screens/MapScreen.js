779760972ba060845ec15c76527b24bdcebf2caa
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-09-05 15:11:48 +0700

    adding activity indicator

--- jobs/screens/MapScreen.js
+++ jobs/screens/MapScreen.js
@@ -1,26 +1,38 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, ActivityIndicator } from 'react-native'
 import { MapView } from 'expo'
 
 class MapScreen extends Component {
-    state = {
-        region: {
-            longitude: -122,
-            latitude: 37,
-            longitudeDelta: 0.04,
-            latitudeDelta: 0.09
-        }
-    }
+  state = {
+    mapLoaded: false,
+    // initial region
+    region: {
+    longitude: -122,
+    latitude: 37,
+    longitudeDelta: 0.04,
+    latitudeDelta: 0.09
+  }
+}
 
-    render() {
-        return (
-            <View style={{ flex: 1 }}>
-              <MapView 
-                region={this.state.region}
-                style={{ flex: 1 }}/>
-            </View>
-        )
+  componentDidMount() {
+      this.setState({ mapLoaded: true })
+  }
+
+  render() {
+    // show indicator when loading map view
+    if (!this.state.mapLoaded) {
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+        <ActivityIndicator size="large" />
+      </View>
     }
+    return (
+      <View style={{ flex: 1 }}>
+        <MapView 
+        region={this.state.region}
+        style={{ flex: 1 }}/>
+      </View>
+    )
+  }
 }
 
 export default MapScreen
