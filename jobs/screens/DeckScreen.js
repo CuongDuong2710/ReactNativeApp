8ec95c1442f19533e5feb1b10cd936f17c90cee1
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-09-06 13:52:53 +0700

    rendering ans styling job card

--- jobs/screens/DeckScreen.js
+++ jobs/screens/DeckScreen.js
@@ -1,17 +1,45 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 import { connect } from 'react-redux'
+import { MapView } from 'expo'
+import { Card, Button } from 'react-native-elements'
 import Swipe from '../components/Swipe'
 
 class DeckScreen extends Component {
+  // receving a single job & return some amount of jsx will show a render card
+  // 'job.snipped' is replace open tag <b> and close tag </b> to empty string ''
+  renderCard(job) {
+    return (
+      <Card title={job.jobtitle}>
+        <View style={styles.detailWrapper}>
+          <Text>{job.company}</Text>
+          <Text>{job.formattedRelativeTime}</Text>
+        </View>
+        <Text>
+          {job.snippet.replace(/<b>/g, '').replace(/<\/b/g, '')}
+        </Text>
+      </Card>
+    )
+  }
   render() {
+    // 'this.renderCard' we're not placing any parenthese on here because we don't want to call it right now.
+    // It's up to the Swipe component for deciding when and how to call the function.
     return (
       <View>
         <Swipe 
           data={this.props.jobs}
+          renderCard={this.renderCard}
         />
       </View>
     )
+  }
+}
+
+const styles = {
+  detailWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginBottom: 10
   }
 }
 
