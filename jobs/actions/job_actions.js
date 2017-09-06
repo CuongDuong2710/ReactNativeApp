4275e982888f1aa861a4a3323869a0a4ec586423
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-09-06 08:40:31 +0700

    converting latlong to zip

--- jobs/actions/job_actions.js
+++ jobs/actions/job_actions.js
@@ -1,10 +1,25 @@
 import axios from 'axios'
+import reverseGeocode from 'latlng-to-zip'
+import qs from 'qs'
 
 import {
     FETCH_JOBS
 } from './types'
 
+const JOB_QUERY_PARAMS = {
+  publisher: '4201738803816157',
+  format: 'json',
+  v: '2', // version
+  latlong: 1, // get back latitude and longitude that we get a job, user can visually see the exact location of job
+  radius: 10, // 10 miles of center latitude and longitude
+  q: 'javascript'
+}
+
 // From 'MapScreen': we're going to pass the entire 'region' object over to the action creator
 export const fetchJobs = (region) => async (dispatch) => {
-
+  try {
+    let zip = reverseGeocode(region)
+  } catch(e) {
+      console.error(e)
+  }
 }
