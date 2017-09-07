329a1931a5ac14f48906f0cfbddb6293826f8766
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-09-07 09:04:46 +0700

    considering only unique jobs

--- jobs/reducers/index.js
+++ jobs/reducers/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux'
 import auth from './auth_reducer'
 import jobs from './jobs_reducer'
+import likedJobs from './likes_reducer'
 
 export default combineReducers({
     // auth: auth
     // the list of jobs exits on the the 'jobs' piece of state and pass in 'DeckScreen' mapStateToProps()
-    auth, jobs
+    auth, jobs, likedJobs
 })
---
+++ jobs/reducers/likes_reducer.js
@@ -0,0 +1,20 @@
+import _ from 'lodash'
+import {
+    LIKE_JOB
+} from '../actions/types'
+
+export default function(state = [], action) {
+  switch (action.type) {
+    case LIKE_JOB:
+      // 'uniqBy' should compare in between all the different jobs inside this array
+      // to make sure that we only get back to a unique list of jobs that comparison key
+      // 'action.payload': the job that the user just liked
+      // ...state: add the list of jobs that the user has previously liked
+      return _.uniqBy([
+        action.payload, ...state
+      ],'jobkey')
+    // Default case which the reducer doesn't care about the action that is getting
+    default:
+      return state
+  }
+}
