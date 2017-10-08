c79e75eb76d340fbcfa63b13a3badbe0448de90a
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-10-08 17:01:10 +0700

    creating library reducer list of data

--- tech/src/reducers/index.js
+++ tech/src/reducers/index.js
@@ -1,7 +1,12 @@
 import { combineReducers } from 'redux'
+import LibraryReducer from './LibraryReducer'
 
 //Turns an object whose values are different reducer functions, into a single reducer function. 
 //It will call every child reducer, and gather their results into a single state object, whose keys correspond to the keys of the passed reducer functions.
+// That makes multiple reducers work together nicely
 export default combineReducers({
-    libraries: () => []
+    libraries: LibraryReducer
 })
+
+// console.log(state.getState())
+// { libraries: [] }
