a6b8d549ee074d00c3d5534a201e42ce8c35f600
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-10-09 18:04:33 +0700

    creating action creator selectedLibrary

---
+++ tech/src/actions/index.js
@@ -0,0 +1,12 @@
+/**
+ * Locate all action creator.
+ * Whenever we call action creator they return to action will be automatically dispatched to us
+ * and sent to all the different reducers.
+ */
+export const selectedLibrary = (libraryId) => {
+  // An action is how we cause the reducers to update the data that they produce.
+  return {
+    type: 'select_library',
+    payload: libraryId
+  }
+}
